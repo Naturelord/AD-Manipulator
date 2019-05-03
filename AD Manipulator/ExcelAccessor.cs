100c1527d5a7adbbc1211db8e0ad1ebf524b01e2
Blake Micheal Patton <jfbmpatton@hotmail.com>
2019-05-02 17:48:57 -0700

    THIS project will be used to Maniuplate AD for Murray Company as requested.

---
+++ AD Manipulator/ExcelAccessor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AD_Manipulator
+{
+    class ExcelAccessor
+    {
+        public Excel.Workbook wb { get; private set; }
+        public Excel.Worksheet Sheet { get; private set; }
+        public Excel.Application excel;
+        private string path { get; set; }
+
+        public ExcelAccessor(string pathGiven)
+        {
+            this.path = pathGiven;
+            Open(path);
+            // [ Row ] [ Column ]
+        }
+
+        /// <summary>
+        /// Must call after you have accessed and pulled data from Excel
+        /// </summary>
+        public void closeOut()
+        {
+            wb.Close();
+            excel.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excel);
+        }
+        public void Save()
+        {
+            wb.Save();
+        }
+        public void Open(string path)
+        {
+            excel = new Excel.Application();
+            wb = excel.Workbooks.Open(path);
+            Sheet = wb.Worksheets[1];
+            MessageBox.Show(Sheet.Cells[1][3].Value2, "Hello");
+        }
+
+
+
+    }
+}
--- AD Manipulator/MainWindow.xaml.cs
+++ AD Manipulator/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.DirectoryServices.AccountManagement;
 
 namespace AD_Manipulator
 {
@@ -20,9 +23,60 @@ namespace AD_Manipulator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string domain = "murraycompany.local";
+        private string password = "";
+        private string domainName = Environment.UserDomainName;
+        private DirectorySearcher ds = null;
+        private string excelpath = @"C:\Egnyte\Private\bpatton\Data\murray_users.xls";
+
+       
+
         public MainWindow()
         {
             InitializeComponent();
+            ExcelAccessor accessor = new ExcelAccessor(excelpath);
+            GetADUsers();
         }
+
+
+        /// <summary>
+        /// Dead Code Must have to not pull Errors... further research into this issue will be looked later.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        private void GetADUsers()
+        {
+            
+            using (var context = new PrincipalContext(ContextType.Domain, "murraycompany.local"))
+            {
+                using (var searcher = new PrincipalSearcher(new UserPrincipal(context)))
+                {
+                    foreach (var result in searcher.FindAll())
+                    {
+                        DirectoryEntry de = result.GetUnderlyingObject() as DirectoryEntry;
+                        var clsuser = new User();
+                        MessageBox.Show(Convert.ToString(de.Properties["samAccountName"].Value) + " , "+ Convert.ToString(de.Properties["givenName"].Value));
+                    }
+                    
+                }
+            }
+        }
+    }
+
+    public class User
+    {
+        public string email { get; set; }
+        public string name { get; set; }
+        public string phoneNumber { get; set; }
     }
 }
