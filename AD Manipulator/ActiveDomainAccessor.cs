df3595db9648867e8b2a0478d9c1451c25c4b614
Blake Micheal Patton <jfbmpatton@hotmail.com>
2019-05-20 16:46:59 -0700

    Changes

---
+++ AD Manipulator/ActiveDomainAccessor.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+using System.Windows;
+
+namespace AD_Manipulator
+{
+    class ActiveDomainAccessor
+    {
+        string filter = "";
+        string ADName = "";
+        string adminUsername = "";
+        string adminPassword = "";
+        DirectoryEntry de = null;
+        DirectorySearcher ds = null;
+
+
+        /// <summary>
+        /// Will Create a new Active Domain Accessor with all the objects in the domain included
+        /// </summary>
+        /// <param name="adName"></param>
+        /// <param name="aduser"></param>
+        /// <param name="adpass"></param>
+        public ActiveDomainAccessor(string adName, string aduser, string adpass)
+        {
+            // Setting global variables for possible future use
+            ADName = adName;
+            adminUsername = aduser;
+            adminPassword = adpass;
+            // Open connection with domain
+            try
+            {
+                de = new DirectoryEntry($"LDAP://{adName}", aduser, adpass);
+                de.AuthenticationType = AuthenticationTypes.Secure;
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            // Search the domain based on no filter to retrieve all objects
+            if (de != null)
+            {
+                DirectorySearcher ds = new DirectorySearcher(de);
+                // Find all
+                SearchResultCollection sr = ds.FindAll();
+            }
+        }
+        /// <summary>
+        /// Will Create a new Active Domain Accessor with only the filtered objects in the domain included.
+        /// </summary>
+        /// <param name="adName"></param>
+        /// <param name="aduser"></param>
+        /// <param name="adpass"></param>
+        /// <param name="filter"></param>
+        public ActiveDomainAccessor(string adName, string aduser, string adpass, string filter)
+        {
+
+        }
+
+        /// <summary>
+        /// Given a unique email address and an AD element - Delete the AD Element from the user's profile
+        /// </summary>
+        /// <param name="adElement"> AD Element you wish to delete</param>
+        /// <param name="userEmail"> Email Address of User (unique email) </param>
+        public void deleteElementFromUser(string adElement, string userEmail)
+        {
+
+        }
+
+        public void findAll()
+        {
+
+        }
+
+        /// <summary>
+        /// Returns the correct form of the number given (without +1), if the number is already correct it returns the given number
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public string correctPhoneNumberNoPlusOne( string number)
+        {
+            string ret = number;
+            if(number != null)
+            {
+                ret = ret.Trim();
+                string check = ret.Substring(0, 2);
+                if( check.CompareTo("+1") == 0)
+                {
+                    ret = ret.Substring(2);
+                }
+            }
+            return ret;
+        }
+
+
+    }// End Class
+}// End Namespace
--- AD Manipulator/MainWindow.xaml.cs
+++ AD Manipulator/MainWindow.xaml.cs
@@ -37,12 +37,72 @@ namespace AD_Manipulator
         public MainWindow()
         {
             InitializeComponent();
-            accessor = new ExcelAccessor(excelpath);
-            changeADBasedOnExcel(accessor.Sheet);
+            //accessor = new ExcelAccessor(excelpath);
+            //changeADBasedOnExcel(accessor.Sheet);
             //+testTwo();
+            removeOnesFromPhoneNumbersInAD();
             MessageBox.Show("Done");
         }
         
+        // New Script to remove All +1's from everyones phone numbers
+        private void removeOnesFromPhoneNumbersInAD()
+        {
+            string murray = "murraycompany.local";
+            // Create the Object of our directoy
+            DirectoryEntry de = new DirectoryEntry($"LDAP://{murray}", "bpatton", "Meganega13");
+            de.AuthenticationType = AuthenticationTypes.Secure;
+            // find the Directory
+            DirectorySearcher ds = new DirectorySearcher(de);
+            ds.Filter= "(&(objectcategory=user))";
+            
+            // Officially assign that directory
+            SearchResultCollection sr = null;
+            try
+            {
+                sr = ds.FindAll();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            
+
+            int i = 0;
+            string add = "";
+            foreach ( SearchResult userResult in sr)
+            {
+                
+                de = userResult.GetDirectoryEntry();
+
+
+
+                string fax = Convert.ToString(de.Properties[ADProperties.FAX].Value);
+                if (!String.IsNullOrEmpty(fax))
+                {
+                    fax = correctPhoneNumberNoPlusOne(fax);
+                    de.Properties[ADProperties.FAX].Value = fax;
+                    de.CommitChanges();
+                }
+
+
+
+                //string show = Convert.ToString(de.Properties[ADProperties.TELEPHONE].Value);
+                //show = show + " " + Convert.ToString(de.Properties[ADProperties.FAX].Value);
+                //show = show + " " + Convert.ToString(de.Properties[ADProperties.NAME].Value);
+                //add = add + show + "\n";
+                //if (i == 24)
+                //{
+                //        MessageBox.Show(add);
+                //        i = 0;
+                //        add = "";
+                //}
+            de.CommitChanges();
+                i++; 
+            }
+            
+
+        }
+
         private void changeADBasedOnExcel(Excel.Worksheet Sheet)
         {
             for (int i = 2; i < 360; i++)
@@ -141,7 +201,21 @@ namespace AD_Manipulator
             }
             return null;
         }
-        
+
+        public string correctPhoneNumberNoPlusOne(string number)
+        {
+            string ret = number;
+            if (number != null)
+            {
+                ret = ret.Trim();
+                string check = ret.Substring(0, 2);
+                if (check.CompareTo("+1") == 0)
+                {
+                    ret = ret.Substring(2);
+                }
+            }
+            return ret;
+        }
 
         /// <summary>
         /// Dead Code Must have to not pull Errors... further research into this issue will be looked later.
